d316d9e1e761b48352e43b2f7c64b7c72054e7e0
gengxiaoyu <727769206@qq.com>
2019-12-09 10:49:29 +0800

    1210lvy大屏复制

---
+++ src/main/java/com/spdb/nrpt/mapper/reportDataMapper/NRPTReportData_1Mapper.java
@@ -0,0 +1,141 @@
+package com.spdb.nrpt.mapper.reportDataMapper;
+
+import com.spdb.nrpt.entity.Unit.ReportUnit;
+import com.spdb.nrpt.entity.bridge.DivIDKeyID;
+import com.spdb.nrpt.entity.procedure.OutData;
+import com.spdb.nrpt.entity.report.ReportEchartsEntity;
+import com.spdb.nrpt.entity.report.ReturnData;
+import com.spdb.nrpt.entity.report.branch.ReportBranchDataEntity;
+import com.spdb.nrpt.entity.report.custom.CustomNumberEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper
+@Component
+@Lazy(value = false)
+public interface NRPTReportData_1Mapper {
+
+    //插入最新数据
+    void insertBatchReportAllData(List<OutData> list);
+
+    //插入留存数据
+    void insertBatchReportPastData(List<OutData> list);
+
+    //初始化插入桥表数据
+    void insertBatchReportBridge(List<DivIDKeyID> list);
+
+    //插入Echarts数据
+    void insertEcharts(ReportEchartsEntity reportEchartsEntity);
+
+    //单个插入ReportPastData
+    void insertReportPastData(OutData outData);
+
+    //批量插入客户数
+    void insetBatchCustomData(List<CustomNumberEntity> list);
+
+    //批量插入分行网点数据
+    void insertBatchReportBranchDataTable(List<ReportBranchDataEntity> list);
+
+    //清空Report_DataTable
+    void truncateReportDataTable();
+
+    //Report_DataTable去重
+    void dropRepeatReportDataTable1();
+    void dropRepeatReportDataTable2();
+    void dropRepeatReportDataTable3();
+    void dropRepeatReportDataTable4();
+
+    //删除部分数据重新初始化
+    void deleteEchartsTableByID(String divID);
+
+    //根据keyID和STATIS_DT删除Report_PastDataTable
+    void deleteReportPastDataTableByKeyIDAndDate(String keyID, String Date);
+
+    //根据STATIS_DT删除Report_PastDataTable
+    void deleteReportPastDataTableByKeyDate(String Date);
+
+    //删除小于某个时间段的keyID在（YS01,YS01010101,YS01010102）
+    void deleteReportPastDataTableByDateInSomeKey(String Date);
+
+    //删除桥表数据
+    void deleteDivKeyTable();
+
+    //依据ParentID，删除网点数据
+    void deleteReportBranchDataTableByParentID(String ParentID);
+
+    //根据divID在桥表中查询keyID
+    List<String> selectKeyIDByDivID(String divID);
+
+    //根据KeyID和组织ID查询数据查询数据
+    ReturnData selectOneReportDataByDivIDAndOrgID(String keyID, String orgID);
+
+    //根据KeyID和组织ID查询数据查询数据
+    List<ReturnData> selectReportDataByDivIDAndOrgID(String keyID, String orgID);
+
+    //根据KeyID查询各分行数据，，不包含9900，9940，（指标名，指标值）
+    List<ReturnData> selectReportDataByDivIDAndNotInOrgID(String keyID);
+
+    //通过KeyID查询各分行数据，，不包含9900，9940（分行名，指标值）
+    List<ReturnData> selectReportDataByDivIDAndOrgIDDesc(String keyID);
+
+    //通过KeyID查询各分行数据，，不包含9900，9940（分行名，指标值）
+    List<ReturnData> selectReportDataByDivIDAndOrgIDAsc(String keyID);
+
+    //批量根据KeyID返回数据，默认组织为9900，查询部分合计数据
+    List<ReturnData> selectBatchReportDataByMoreKeyID(List<String> list);
+
+    //批量根据OrgID和KeyID返回数据，默认组织为9900，查询部分合计数据
+    List<ReturnData> selectBatchReportDataByMoreOrgIDAndKeyID(String keyID, List<String> orgID);
+
+    //批量根据KeyID返回数据，查询全部合计数据
+    List<OutData> selectBatchDataReportAllDataByDivIDAndOrgID(List<String> list);
+
+    //根据KeyID查询过往数据（指标名，指标值），时间降序
+    List<ReturnData> selectReportPastDataByDivIDAndOrgID(String keyID, String orgID);
+
+    //根据KeyID查询过往数据（日期，指标值），时间降序
+    List<ReturnData> selectReportPastDateDataByDivIDAndOrgID(String keyID);
+
+    //根据KeyID查询过往数据（日期，指标值），时间升序
+    List<ReturnData> selectReportPastDateDataByDivIDAndOrgIDAsc(String keyID, String orgID);
+
+    //根据KeyID查询过往数据（日期，指标值），按值升序
+    List<ReturnData> selectReportMinPastDateDataByDivIDAndOrgID(String keyID, String orgID);
+
+    //根据divID返回该分区名字跟Echarts组件参数
+    ReportEchartsEntity selectOptionByID(String divID);
+
+    //根据Cust_Type_Cd，客户数特殊查询
+    List<ReturnData> selectCustomerNumberByCust_Type_Cd(String Cust_Type_Cd, String orgID);
+
+    //根据divID和orgID查询单位
+    ReportUnit selectReportUnitByOrgID(String divID, String orgID);
+
+    //根据ParentID，KPI_ID，查询分行网点数据,KPI_VALUE，降序
+    List<ReturnData> selectDescReportBranchByParentIDAndKPIID(String keyID, String ParentID);
+
+    //根据ORG_ID，KPI_ID，查询分行网点数据，KPI_VALUE，降序
+    ReturnData selectDescReportBranchByOrgIDAndKPIID(String keyID, String orgID);
+
+    //根据ParentID，KPI_ID，查询分行网点数据,KPI_VALUE，升序,值可以为空
+    List<ReturnData> selectAscReportBranchByParentIDAndKPIID(String keyID, String ParentID);
+
+    //根据ParentID，KPI_ID，查询分行网点数据,KPI_VALUE，升序,值不可以为空
+    List<ReturnData> selectAscReportBranchByParentIDAndKPIIDNotNull(String keyID, String ParentID);
+
+    //根据parentOrgID查询网点机构号
+    List<String> selectReportOrgDataTableByParentID(String parentOrgID);
+    
+    //根据orgName在Report_OrgDataTable中查询orgID
+    String selectReportOrgDataTableByOrgName(String orgName);
+
+
+    //查询第三屏下方新增前两个指标
+    List<String> selectThirdThreeIndex(OutData outData);
+    //查询第三屏最后一个指标
+    List<String> selectFinBal(OutData outData);
+
+}
---
+++ src/main/java/com/spdb/nrpt/service/report/NRPTReport_1Service.java
@@ -0,0 +1,155 @@
+package com.spdb.nrpt.service.report;
+
+import com.alibaba.fastjson.JSONObject;
+import com.spdb.nrpt.entity.div.DivEntity;
+import com.spdb.nrpt.entity.echarts.AxisEntity;
+import com.spdb.nrpt.entity.report.ReportEchartsEntity;
+import com.spdb.nrpt.entity.report.ReturnData;
+import com.spdb.nrpt.mapper.reportDataMapper.NRPTReportDataMapper;
+import com.spdb.nrpt.mapper.reportDataMapper.NRPTReportData_1Mapper;
+import com.spdb.nrpt.util.TimeUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@Slf4j
+public class NRPTReport_1Service {
+
+    @Autowired
+    private NRPTReportData_1Mapper nrptReportDataMapper;
+
+    @Autowired
+    private NRPTDealData_1Service nrptDealDataService;
+
+    @Autowired
+    private NRPTReport1_1Service nrptReport1Service;
+
+    @Autowired
+    private NRPTReport2_1Service nrptReport2Service;
+
+    @Autowired
+    private NRPTReport3_1Service nrptReport3Service;
+
+    //获得divEcharts组件参数
+    public Object getDivEcharsData(String divID,String orgID){
+        if ("100109".equals(divID)){
+            if (!("9900".equals(orgID))){
+                divID="100109"+orgID;
+            }
+        }
+        ReportEchartsEntity reportEchartsEntity = nrptReportDataMapper.selectOptionByID(divID);
+        String jsonData = reportEchartsEntity.getDivEchartsJson();
+        String type = reportEchartsEntity.getDivEchartsType();
+        if (!("none".equals(jsonData))){
+            try {
+                Object option = JSONObject.parseObject(jsonData, Class.forName(type));
+                log.info("返回出去的实体类是 ： ",option.toString());
+                return option;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    //大屏数据特殊处理
+    public DivEntity dealData(String key, String divID, DivEntity divEntity,String orgID){
+        List<String> keyIDs = getKeyIDByDivID(divID);
+        if ("大屏一全部数据".equals(key)){
+            List<List<ReturnData>> data =  nrptReport1Service.getDivDataByKeyID(divID,keyIDs,orgID);
+            if (data==null){
+                if ("100108".equals(divID)){
+                    divEntity.setData(nrptReport1Service.get100108Data(keyIDs,orgID));
+                }else if ("100109".equals(divID)){
+                    divEntity.setData(nrptReport1Service.get100109Data(keyIDs,orgID));
+                }else if ("100104".equals(divID)){
+                    divEntity.setData(nrptReport1Service.get100104Data(keyIDs,orgID));
+                }
+            }else {
+                divEntity.setData(data);
+                if(data.size()==1&&(!"100109".equals(divID))||"100102".equals(divID)){
+                    if ("100102".equals(divID)){
+                        List<String> axisList = Arrays.asList("今日","上日","上月");
+                        divEntity.setAxis(axisList);
+                    } else {
+                        List<String> axisList = new ArrayList<>();
+                        for (ReturnData returnData : data.get(0)){
+                            String name = returnData.getName();
+                            axisList.add(name);
+                        }
+                        divEntity.setAxis(axisList);
+                        if ("100107".equals(divID)){
+                            AxisEntity axisEntity = new AxisEntity();
+                            axisEntity.setData(axisList);
+                            axisEntity.setMin(nrptReportDataMapper.selectReportMinPastDateDataByDivIDAndOrgID("YS01",orgID).get(0).getValue());
+                            divEntity.setAxis(axisEntity);
+                        }
+                    }
+                }
+            }
+        }else if ("大屏二全部数据".equals(key)){
+            List<ReturnData> data2 = nrptReport2Service.getDivDataByKeyID(divID,keyIDs,orgID);
+            if (data2==null){
+                if ("100207".equals(divID)){
+                    divEntity.setData(nrptReport2Service.get100207Data(keyIDs,orgID));
+                    List<String> timeList = TimeUtil.getSixMonthLastDay();
+                    divEntity.setAxis(timeList);
+                }else if("100204".equals(divID)){
+                    divEntity.setData(nrptReport2Service.get100204Data(keyIDs,orgID));
+                    AxisEntity axisEntity = new AxisEntity();
+                    axisEntity.setData(nrptDealDataService.trendAxis());
+                    axisEntity.setMin(nrptReportDataMapper.selectReportMinPastDateDataByDivIDAndOrgID("YS01010101",orgID).get(0).getValue());
+                    divEntity.setAxis(axisEntity);
+                }
+            }else {
+                divEntity.setData(data2);
+                if ("100201".equals(divID)||"100202".equals(divID)){
+                    List<String> axisList = new ArrayList<>();
+                    for (ReturnData returnData : data2){
+                        String name = returnData.getName();
+                        axisList.add(name);
+                    }
+                    divEntity.setAxis(axisList);
+                }
+            }
+        }else if ("大屏三全部数据".equals(key)){
+            List<ReturnData> data3 = nrptReport3Service.getDivDataByKeyID(divID,keyIDs,orgID);
+            if (data3==null){
+                if ("100307".equals(divID)){
+                    divEntity.setData(nrptReport3Service.get100307Data(keyIDs,orgID));
+                    List<String> timeList = TimeUtil.getSixMonthLastDay();
+                    divEntity.setAxis(timeList);
+                }else if ("100304".equals(divID)){
+                    divEntity.setData(nrptReport3Service.get100304Data(keyIDs,orgID));
+                    AxisEntity axisEntity = new AxisEntity();
+                    axisEntity.setData(nrptDealDataService.trendAxis());
+                    axisEntity.setMin(nrptReportDataMapper.selectReportMinPastDateDataByDivIDAndOrgID("YS01010102",orgID).get(0).getValue());
+                    divEntity.setAxis(axisEntity);
+                }
+            }else {
+                divEntity.setData(data3);
+                if ("100301".equals(divID)||"100302".equals(divID)){
+                    List<String> axisList = new ArrayList<>();
+                    for (ReturnData returnData : data3){
+                        String name = returnData.getName();
+                        axisList.add(name);
+                    }
+                    divEntity.setAxis(axisList);
+                }
+            }
+        }
+        return divEntity;
+    }
+
+    //通过divID获得keyID
+    public List<String> getKeyIDByDivID(String divID){
+        return nrptReportDataMapper.selectKeyIDByDivID(divID);
+    }
+
+}
+
