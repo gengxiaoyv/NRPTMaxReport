d2dbfb9519ea35161da9f714a4e32b0f57b663a5
gengxiaoyu <727769206@qq.com>
2019-12-25 17:17:06 +0800

    redis

---
+++ src/main/java/com/spdb/nrpt/config/redisConfig/RedisContext.java
@@ -0,0 +1,15 @@
+package com.spdb.nrpt.config.redisConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RedisContext {
+
+
+    public static final String RETAIL2DEP = "retail2dep";
+    public static final String RETAIL2LOAN = "retail2loan";
+    public static final String RETAIL2FINASS= "retail2finass";
+    public static final String RETAIL2NODEP = "retail2nodep";
+    public static final String RETAIL2DEPINANDOUT = "retail2depinandout";
+
+}
